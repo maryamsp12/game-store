b80dcec48a16efca5bb6b4b3509d91e33b2aff50
unknown <maryamsp12@gmail.com>
2026-05-07 13:54:11 +0300

    Initial responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,56 @@
-import Button from '@mui/material/Button';
+import { Grid, Box } from "@mui/material";
 
 export default function App() {
   return (
-    <div>
-      <Button variant="contained">
-        Game Store
-      </Button>
-    </div>
+    <Grid
+      container
+      sx={{
+        display: "grid",
+        gridTemplateAreas: {
+          xs: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        },
+        gridTemplateColumns: {
+          xs: "1fr",
+          lg: "250px 1fr",
+        },
+        gridTemplateRows: "auto 1fr",
+        minHeight: "100vh",
+      }}
+    >
+      {/* Nav */}
+      <Box
+        sx={{
+          gridArea: "nav",
+          backgroundColor: "coral",
+          p: 2,
+        }}
+      >
+        Nav
+      </Box>
+
+      {/* Aside (only show on lg) */}
+      <Box
+        sx={{
+          gridArea: "aside",
+          backgroundColor: "gold",
+          p: 2,
+          display: { xs: "none", lg: "block" },
+        }}
+      >
+        Aside
+      </Box>
+
+      {/* Main */}
+      <Box
+        sx={{
+          gridArea: "main",
+          backgroundColor: "dodgerblue",
+          p: 2,
+        }}
+      >
+        Main
+      </Box>
+    </Grid>
   );
 }
