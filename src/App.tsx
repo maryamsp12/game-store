3c246bcd9b03e122b691ae13ba041280425aa82f
unknown <maryamsp12@gmail.com>
2026-05-07 12:54:20 +0300

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+
+export default function App() {
+  return (
+    <div>
+      <Button variant="contained">
+        Game Store
+      </Button>
+    </div>
+  );
+}
