961c88453c2e5ebc713e6a00377a2b0c1569f5be
unknown <maryamsp12@gmail.com>
2026-05-08 15:44:10 +0300

    implement dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './theme.ts';
+import { CssBaseline } from '@mui/material';
 
-const theme = createTheme({});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </StrictMode>,
