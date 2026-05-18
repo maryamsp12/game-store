f82f31ab3fc21e84707300d4e1080c71e913a882
unknown <maryamsp12@gmail.com>
2026-05-18 11:53:21 +0300

    displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -6,6 +6,7 @@ import {
   Typography,
 } from "@mui/material";
 import type { Game } from "../hooks/useGames";
+import PlatformIconsList from "./PlatformIconsList";
 
 interface Props {
   game: Game;
@@ -13,7 +14,14 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card
+      sx={{
+        maxWidth: 345,
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <CardActionArea>
         <CardMedia
           component="img"
@@ -23,7 +31,14 @@ const GameCard = ({ game }: Props) => {
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            game.name
+            {game.name}
+          </Typography>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+              <PlatformIconsList
+                platforms={game.parent_platforms.map((pp) => pp.platform)}
+              />
+            </ul>
           </Typography>
         </CardContent>
       </CardActionArea>
---
+++ src/components/PlatformIconsList.tsx
@@ -0,0 +1,42 @@
+import type { Platform } from "../hooks/useGames";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+  FaGamepad,
+} from "react-icons/fa";
+
+import { MdPhoneIphone } from "react-icons/md";
+import { BsGlobe } from "react-icons/bs";
+import { Stack } from "@mui/system";
+import { grey } from "@mui/material/colors";
+
+interface Props {
+  platforms: Platform[];
+}
+const PlatformIconsList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: React.ReactNode } = {
+    pc: <FaWindows />,
+    playstation: <FaPlaystation />,
+    xbox: <FaXbox />,
+    nintendo: <FaGamepad />,
+    mac: <FaApple />,
+    linux: <FaLinux />,
+    android: <FaAndroid />,
+    ios: <MdPhoneIphone />,
+    web: <BsGlobe />,
+  };
+
+  return (
+    <Stack direction="row" spacing={1} marginY={1}>
+      {platforms.map((p: Platform) => (
+        <span key={p.id} style={{ color: grey[500], fontSize: 18 }}>{iconMap[p.slug]}</span>
+      ))}
+    </Stack>
+  );
+};
+
+export default PlatformIconsList;
