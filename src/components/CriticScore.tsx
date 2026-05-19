abf3f68448ffc0200bdf35c81615ae6b99ebfc2d
unknown <maryamsp12@gmail.com>
2026-05-19 13:15:42 +0300

    display critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,17 @@
+import { Chip } from "@mui/material";
+interface Props {
+  score: number;
+}
+const CriticScore = ({ score }: Props) => {
+  const getColor = () => {
+  if (score > 75) return "success";
+  if (score > 60) return "warning";
+  return "error";
+};
+  return <Chip
+  label={score}
+  color={getColor()}
+/>;
+};
+
+export default CriticScore;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,9 +4,11 @@ import {
   CardMedia,
   CardContent,
   Typography,
+  Stack,
 } from "@mui/material";
 import type { Game } from "../hooks/useGames";
 import PlatformIconsList from "./PlatformIconsList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -30,15 +32,30 @@ const GameCard = ({ game }: Props) => {
           alt="green iguana"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography
+            gutterBottom
+            variant="h6"
+            component="div"
+            sx={{
+              minHeight: 64,
+            }}
+          >
             {game.name}
           </Typography>
           <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+            {/* <ul style={{ listStyle: "none", padding: 0, margin: 0 }}> */}
+            <Stack
+              direction="row"
+              spacing={1}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+            >
               <PlatformIconsList
                 platforms={game.parent_platforms.map((pp) => pp.platform)}
               />
-            </ul>
+              <CriticScore score={game.metacritic} />
+            </Stack>
+            {/* </ul> */}
           </Typography>
         </CardContent>
       </CardActionArea>
