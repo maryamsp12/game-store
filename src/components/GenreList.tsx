7eb1de5cf1347d222cd533f901803d3c4c6c4e14
unknown <maryamsp12@gmail.com>
2026-05-30 14:54:13 +0300

    display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,26 @@
-import useGenres from '../hooks/useGenres';
+import { List, ListItem, Stack, Typography } from "@mui/material";
+import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-    const { data: genres, error, isLoading } = useGenres();
-  return (
-    <ul>{genres.map((genre) => (
-      <li key={genre.id}>{genre.name}</li>
-    ))}</ul>
-  )
-}
+  const { data: genres, error, isLoading } = useGenres();
 
-export default GenreList
+  return (
+    <List>
+      {genres.map((genre) => (
+        <ListItem key={genre.id}>
+          <Stack direction="row" spacing={2}>
+            <img
+              src={getCroppedImageUrl(genre.image_background)}
+              alt={genre.name}
+              style={{ width: 32, height: 32, borderRadius: 8 }}
+            />
+            <Typography variant="body1">{genre.name}</Typography>
+          </Stack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default GenreList;
