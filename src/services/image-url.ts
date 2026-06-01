23e393cb04cf38e8a716c6113657168cfe2c616e
unknown <maryamsp12@gmail.com>
2026-06-01 13:52:07 +0300

    added placeholder image for games with images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,9 +1,10 @@
-const getCroppedImageUrl = (url: string) => {
-    if (!url) return null;
-    const index = url.indexOf('media/') + 'media/'.length;
-    url.slice(0, index)
-    return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+import noImage from "../assets/no-image.webp";
 
-}
+const getCroppedImageUrl = (url: string) => {
+  if (!url) return noImage;
+  const index = url.indexOf("media/") + "media/".length;
+  url.slice(0, index);
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
 
 export default getCroppedImageUrl;
